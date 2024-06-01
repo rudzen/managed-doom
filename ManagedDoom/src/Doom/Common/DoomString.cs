4285aaf10b15bffb90212629115dccde71a13162
Rudy Alex Kohn <rudzen@gmail.com>
2024-06-01 19:59:50 +0200

    restructure a few files to ease the pain

--- ManagedDoom/src/Doom/Common/DoomString.cs
+++ ManagedDoom/src/Doom/Common/DoomString.cs
@@ -14,7 +14,6 @@
 //
 
 
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
---
+++ ManagedDoom/src/ManagedDoom.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ManagedDoom;
+using ManagedDoom.Silk;
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine(ApplicationInfo.Title);
+Console.ResetColor();
+
+try
+{
+    string quitMessage;
+
+    using (var doom = new SilkDoom(new CommandLineArgs(args)))
+    {
+        doom.Run();
+        quitMessage = doom.QuitMessage;
+    }
+
+    if (!string.IsNullOrWhiteSpace(quitMessage))
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(quitMessage);
+        Console.ResetColor();
+        Console.Write("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
+catch (Exception e)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(e);
+    Console.ResetColor();
+    Console.Write("Press any key to exit.");
+    Console.ReadKey();
+}
