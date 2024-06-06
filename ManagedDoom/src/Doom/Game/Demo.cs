cfe1075c37644e0a5fd367eca54bc0f1739f511e
Rudy Alex Kohn <rudzen@gmail.com>
2024-06-06 08:03:26 +0200

    simplify demo options create

--- ManagedDoom/src/Doom/Game/Demo.cs
+++ ManagedDoom/src/Doom/Game/Demo.cs
@@ -35,15 +35,17 @@ namespace ManagedDoom.Doom.Game
 
             this.data = data;
 
-            Options = new GameOptions();
-            Options.Skill = (GameSkill)data[p++];
-            Options.Episode = data[p++];
-            Options.Map = data[p++];
-            Options.Deathmatch = data[p++];
-            Options.RespawnMonsters = data[p++] != 0;
-            Options.FastMonsters = data[p++] != 0;
-            Options.NoMonsters = data[p++] != 0;
-            Options.ConsolePlayer = data[p++];
+            Options = new GameOptions
+            {
+                Skill = (GameSkill)data[p++],
+                Episode = data[p++],
+                Map = data[p++],
+                Deathmatch = data[p++],
+                RespawnMonsters = data[p++] != 0,
+                FastMonsters = data[p++] != 0,
+                NoMonsters = data[p++] != 0,
+                ConsolePlayer = data[p++]
+            };
 
             Options.Players[0].InGame = data[p++] != 0;
             Options.Players[1].InGame = data[p++] != 0;
